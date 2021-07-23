57d13ae5cefb8e396177b36c72782a0b699941f2
Janet Liang <jan.liang96@gmail.com>
2021-07-22 19:58:46 -0700

    [#9] Read tx receipt log to display rate for transactions 	* Does not work for provide liquidity transactions yet 	* Needs edge case testing

--- collect_tx_history.py
+++ collect_tx_history.py
@@ -5,28 +5,81 @@ import threading
 import utils
 
 
+market_abi_file = './abi/CashMarket.json'
+market_address = '0x56fe9b91db8d72d6660ad4623459ccb72095cd4b'
+
+
 def process_block(block_num, w3, out):
     block = utils.get_block(block_num, w3)
 
     for tx in block.transactions:
-        v_print(tx)
-        out.put(tx)
+        out.put((block.timestamp, tx))
 
 
-valid_addresses = ['0x56fe9b91db8d72d6660ad4623459ccb72095cd4b']
-def process_tx(hash, w3, out):
+valid_addresses = [market_address]
+def process_tx(data, w3, events, out):
+    time, hash = data
     receipt = utils.get_receipt(hash, w3)
 
-    v_print(f'{receipt.to} // {valid_addresses[0]}')
     if receipt.to is not None and receipt.to.lower() in valid_addresses:
         tx = {}
         tx['from'] = receipt['from']
-        tx['hash'] = hash.hex()
         tx['to'] = receipt.to
         tx['block'] = receipt.blockNumber
-        # TODO: Figure out how to read the tx logs
+        tx['time'] = time
 
-        out.put(tx)
+        result = process_logs(market_address, receipt, events, w3)
+        if result is not None:
+            name, maturity, fCash, cash = result
+            tx['type'] = name
+            tx['maturity'] = maturity
+            tx['fCash'] = str(fCash)
+            tx['cash'] = str(cash)
+            if name == 'TakefCash' or name == 'TakeCurrentCash':
+                tx['rate'] = str(((fCash - cash) / cash))
+            else:
+                tx['rate'] = 'N/A'
+            # Note: Do not add transaction if does not contain a valid event log
+            out.put((hash.hex(), tx))
+
+
+valid_events = ['TakefCash', 'TakeCurrentCash', 'AddLiquidity', 'RemoveLiquidity']
+def process_logs(address, receipt, events, w3):
+    contract = w3.eth.contract(utils.convert_address(address), abi = abi)
+
+    result = None
+    # Note: Existing loop on first log for CashMarket
+    logs = receipt.logs
+    for l in logs:
+        if l['address'] != market_address:
+            continue
+        receipt_hex = w3.toHex(l['topics'][0])
+        name = events.get(receipt_hex, None)
+
+        if name is not None and name in valid_events:
+            # Note: processReceipt returns a tuple with, may need to be changed for later
+            decoded = contract.events[name]().processReceipt(receipt)[0]
+            a = decoded.args
+            result = (name, a.maturity, a.fCash, a.cash)
+            break
+    return result
+
+
+def get_contract_events(address, w3):
+    contract = w3.eth.contract(utils.convert_address(address), abi = abi)
+    events = [event for event in contract.abi if event['type'] == 'event']
+
+    event_sigs = {}
+    for e in events:
+        name = e['name']
+        inputs = [param['type'] for param in e['inputs']]
+        inputs = ','.join(inputs)
+
+        event_sig = f'{name}({inputs})'
+        event_hex = w3.toHex(w3.keccak(text = event_sig))
+        event_sigs[event_hex] = name
+
+    return event_sigs
 
 
 if __name__ == '__main__':
@@ -49,13 +102,12 @@ if __name__ == '__main__':
 
     w3 = utils.set_endpoint(args.endpoint)
     db = utils.get_db(args.credentials)
+    abi = utils.load_abi(market_abi_file)
+
+    contract_events = get_contract_events(market_address, w3)
 
     user_ref = db.collection('users')
     docs = user_ref.stream()
-
-    doc_id = ''
-    for doc in docs:
-        doc_id = doc.id
 
     batch_size = 20
     total_blocks = args.end - args.start
@@ -81,8 +133,8 @@ if __name__ == '__main__':
         tx_threads = []
         store_queue = queue.Queue()
         while not receipt_queue.empty():
-            tx_hash = receipt_queue.get()
-            t = threading.Thread(target = process_tx, args = (tx_hash, w3, store_queue))
+            data = receipt_queue.get()
+            t = threading.Thread(target = process_tx, args = (data, w3, contract_events, store_queue))
             tx_threads.append(t)
         for tt in tx_threads:
             tt.start()
@@ -91,18 +143,17 @@ if __name__ == '__main__':
 
         dataset = {}
         while not store_queue.empty():
-            tx = store_queue.get()
+            hash, tx = store_queue.get()
             sender = tx['from'].lower()
             if dataset.get(sender, None) is None:
                 dataset[sender] = {}
                 dataset[sender]['txs'] = {}
-            dataset[sender]['txs'][tx['hash']] = {}
-            dataset[sender]['txs'][tx['hash']]['to'] = tx['to']
-            dataset[sender]['txs'][tx['hash']]['blockNum'] = tx['block']
+            dataset[sender]['txs'][hash] = tx
 
-        doc_ref = user_ref.document(doc_id)
-        if not args.no_update:
-            doc_ref.set(dataset, merge = True)
-        else:
-            v_print(f'[INFO] Skipping db update.')
-            v_print(dataset)
+        for user in dataset.keys():
+            doc_ref = user_ref.document(user)
+            if not args.no_update:
+                doc_ref.set(dataset[user], merge = True)
+            else:
+                v_print(f'[INFO] Skipping db update.')
+                v_print(dataset)
